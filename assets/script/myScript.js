d5508c50b16947664c4c580ee1a446aa77b4ac90
Abdul Haseeb <haseeb7063952@gmail.com>
2024-05-09 20:20:38 +0500

    new project added

---
+++ assets/script/myScript.js
@@ -0,0 +1,8 @@
+function toggleCalendar() {
+    var calendar = document.getElementById("calendar");
+    if (calendar.style.display === "none") {
+      calendar.style.display = "block";
+    } else {
+      calendar.style.display = "none";
+    }
+  }
